46663db69d7ac658bf5521a525b48938f67bfa30
David Cohen <dcohen@hashicorp.com>
2019-07-07 10:10:05 -0400

    add an IAM role that allows describeInstances for nginx, so it can auto-join consul

--- nginx/main.tf
+++ nginx/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "nginx" {
   instance_type           = "${var.instance_type}"
   key_name                = "${var.key_name}"
   subnet_id               = "${var.subnet_id}"
+  iam_instance_profile    = "${aws_iam_instance_profile.nginx.name}"
   user_data               = "${data.template_file.nginx_userdata.rendered}"
   vpc_security_group_ids  = ["${aws_security_group.nginx.id}"]
 
@@ -93,9 +94,9 @@ data "template_file" "consul_client_config" {
 }
 
 
-###################################################
-# A security group for our nginx-server instances #
-###################################################
+############################################
+# A security group for our nginx instances #
+############################################
 resource "aws_security_group" "nginx" {
   name   = "nginx"
   vpc_id = "${var.vpc_id}"
@@ -122,4 +123,51 @@ resource "aws_security_group" "nginx" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+################################################################
+# IAM Role & Instance Profile that lets us use cloud auto-join #
+################################################################
+resource "aws_iam_instance_profile" "nginx" {
+    name = "nginx"
+    role = "${aws_iam_role.nginx.name}"
+}
+
+resource "aws_iam_role_policy" "nginx" {
+    name = "nginx"
+    role = "${aws_iam_role.nginx.name}"
+    policy = <<EOF
+{
+    "Statement": [
+        {
+            "Sid": "nginxautojoin",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstances"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "nginx" {
+    name = "Nginx"
+    path = "/"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
