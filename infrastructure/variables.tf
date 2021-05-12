64d8036495f0769b265ea5524ecfc73f01142cb8
Dave Cohen <dave@tutorialinux.com>
2021-05-12 17:05:50 +0200

    Update base ubuntu image

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -23,7 +23,8 @@ variable "private_subnet_az" {
   default         = "us-west-2b"
 }
 
+# The AWS Marketplace is awash in scams, use https://cloud-images.ubuntu.com/locator/ec2/
 variable "base_ec2_ami" {
-  # us-west-2 ubuntu 20.04 HVM SSD
-  default = "ami-07dd19a7900a1f049"
+  # us-west-2 ubuntu 20.04 HVM SSD, updated 05-10-2021
+  default = "ami-014a542cf4d33b681"
 }
