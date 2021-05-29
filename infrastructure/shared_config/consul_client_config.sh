88a4ce88cc7484db60e333d82506a9a38ac3d2fa
Dave Cohen <dave@tutorialinux.com>
2021-05-29 13:07:35 -0400

    fix consul on nomad, and consul agent everywhere

--- infrastructure/shared_config/consul_client_config.sh
+++ infrastructure/shared_config/consul_client_config.sh
@@ -1,17 +1,15 @@
 # Consul client config
 mkdir -p /etc/consul.d
-cat <<EOF > '/etc/consul.d/client.json'
-{
-  "datacenter": "tutorialinux",
-  "data_dir": "/var/lib/consul",
-  "retry_join": [
-    "provider=aws tag_key=role tag_value=consul-server"
-  ],
-  "client_addr": "0.0.0.0",
-  "bind_addr": "{{GetInterfaceIP \"eth0\" }}",
-  "leave_on_terminate": true,
-  "enable_syslog": true,
-  "disable_update_check": true,
-  "enable_debug": true
-}
+cat <<EOF > '/etc/consul.d/consul.hcl'
+"datacenter" = "tutorialinux"
+"data_dir" = "/var/lib/consul"
+"retry_join" = ["provider=aws tag_key=role tag_value=consul-server"]
+"client_addr" = "0.0.0.0"
+"bind_addr" = "{{GetInterfaceIP \"eth0\" }}"
+"leave_on_terminate" = true
+"enable_syslog" = true
+"disable_update_check" = true
+"enable_debug" = true
 EOF
+
+systemctl restart consul
