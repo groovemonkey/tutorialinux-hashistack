7344c22e34c05ce26739762d4bce20f15fdffa87
David Cohen <dcohen@hashicorp.com>
2022-05-02 11:50:14 -0400

    try to fix apt race conditions preventing consul install succeeding

--- infrastructure/shared_config/install_base_packages.sh
+++ infrastructure/shared_config/install_base_packages.sh
@@ -1,7 +1,12 @@
 DEBIAN_FRONTEND=noninteractive
 
 echo "Starting system update..."
-apt-get update && apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
+
+# getting around some very weird bugs with this
+apt-get update
+sleep 10
 
 echo "Installing packages..."
 apt-get install -y wget unzip curl
